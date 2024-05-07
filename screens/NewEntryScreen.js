87f7db6af0b5307ff669b8899483c694974b34ee
MT <113047767+Mathias-Thomsen@users.noreply.github.com>
2024-05-07 23:05:22 +0200

    added screens

---
+++ screens/NewEntryScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function NewEntryScreen() {
+  return (
+    <View>
+      <Text>Create a New Journal Entry</Text>
+      {/* Placeholder: Form or text input for journaling would go here */}
+    </View>
+  );
+}
